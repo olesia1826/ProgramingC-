f212c50130659e3fa175099f6ad4e4b7daa9ec9c
olesia1826 <144419157+olesia1826@users.noreply.github.com>
2024-03-15 10:55:56 +0200

    Add files via upload

---
+++ modul1.cs
@@ -0,0 +1,25 @@
+﻿//Створіть клас Matrix, який представляє цілочисельну матрицю. В цьому класі створіть:
+//Зубчастий двовимірний масив цілих чисел.
+//Індексатор для доступу до елементів матриці за допомогою індексів row та column.
+//
+using System;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+public class Matrixs()
+   
+{
+  public   int rows ;
+
+    public int[][] Matrix { get => matrix; set => matrix = value; }
+  
+
+    int cols = 0;
+    int min = 2;
+    int max = 4;
+    private int[][] matrix;
+    public int this[int index] { get { return cols[index]; } set { array[index] = value; } }
+    public  method FillMatrixRandom(int rows, int columns, int min, int max) { 
+
+    };
+}
+
